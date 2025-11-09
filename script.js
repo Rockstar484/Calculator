101bdc2e2b8d995c94f6e1a1ed9c786816a87ad0
Rockstar484 <shivanshg484@gmail.com>
2025-11-09 15:01:23 +0530

    Update

--- script.js
+++ script.js
@@ -1,6 +1,52 @@
-function append(input){
-    document.getElementById("display").value+=input;
+let result=0, expression=0;
+dis = document.getElementById("display")
+
+function append(input,type,multiply){
+    if (dis.value==0) {
+        if (type=='num') {
+        dis.value=input;
+        expression+=input;
+        }
+    }
+    else{
+        if (dis.value.charAt(0)=='=') {
+            clearDisplay();
+            dis.value='';
+        }
+        if (type=='operator') {
+            if (expression[expression.length-1]=='+' || expression[expression.length-1]=='-' || expression[expression.length-1]=='/' || expression[expression.length-1]=='x' || expression[expression.length-1]=='.') {
+                expression[expression.length-1]=input;
+                if (multiply==1) {
+                    dis.value='x';
+                }
+                else{
+                    dis.value=input;
+                }
+            }
+            else {
+                if (multiply==1) {
+                    dis.value='x';
+                    expression+=input;
+                }
+                else {
+                    dis.value=input;
+                    expression+=input;
+                }
+            }
+        }
+        else {
+            dis.value+=input;
+            expression+=""+input;
+        }
+    }
 }
+
 function clearDisplay(){
-    document.getElementById("display").value="0";
+    dis.value="0";
+    expression=0;
+}
+
+function equalSign(){
+    result = eval(expression);
+    dis.value="="+result;
 }
